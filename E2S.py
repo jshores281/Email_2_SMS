c34a69389bec20fd676496a09d150d03bc869d6d
jshores281 <52839097+jshores281@users.noreply.github.com>
2021-07-20 19:03:41 -0500

    Add files via upload

--- E2S.py
+++ E2S.py
@@ -8,12 +8,16 @@ import smtplib
 
 EMAIL_SENDER = input("EMAIL: ")
 EMAIL_PASSWORD = input("PASSWORD: ")
+
 EMAIL_SRVR = input("SMTP SERVER: ")
 EMAIL_PORT = input("SMTP SERVER PORT: ")
+
 RECV_PHONE = input("CELL PHONE NUMBER: ")
 SMS_GATE = input("SMS-GATEWAY: ")
 SUBJECT = input("title of sms: ")
 BODY = input("type message: ")
+
+
 
 EMAIL_ADDR = EMAIL_SENDER
 EMAIL_PASS = EMAIL_PASSWORD
@@ -27,41 +31,39 @@ print("\n", "PRESS ENTER IF CORRECT OR CTRL+C", "\n"*2,
 	"SERVER = %s" % EMAIL_SRVR,"\n",
 	"PORT = %s" % EMAIL_PORT,"\n",
 	"PHONE & SMS-GATEWAY = %s" % RECV_ADDR,"\n"*2,
-      	"TITLE = %s" % SUBJECT,'\n',
-      	"MESSAGE = %s" % BODY,'\n\)
+    "TITLE = %s" % SUBJECT,'\n',
+    "MESSAGE = %s" % BODY,'\n')
 
 
 input()
 
 
 # email to sms function
-def notify_user(EMAIL_ADDR, EMAIL_PASS, RECV_ADDR, SUBJECT, BODY):
-
+def notify_user(EMAIL_ADDR, EMAIL_PASS, RECV_ADDR, EMAIL_SRVR, EMAIL_PORT, SUBJECT, BODY):
 	# make this reprogrammable based on email service provider (smtp server, port)
-	with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+	with smtplib.SMTP(EMAIL_SRVR, EMAIL_PORT) as smtp:
 		smtp.ehlo()
 		smtp.starttls()
 		smtp.ehlo()
 		smtp.login(EMAIL_ADDR, EMAIL_PASS)
 		login_log = smtp.login(EMAIL_ADDR, EMAIL_PASS)
-
-		# make this reprogrammable based on product and values
-		subject = SUBJECT
-		body = BODY
+		print(login_log)
 
 		# email message packer
-		msg = ("From: %s\r\n" % EMAIL_ADDR
+		MSG = ("From: %s\r\n" % EMAIL_ADDR
              + "To: %s\r\n" % RECV_ADDR
-             + "Subject: %s\r\n" % subject
+             + "Subject: %s\r\n" % SUBJECT
              + "\r\n"
-             + body)
+             + BODY)
 
 		# email send function
-		smtp.sendmail(EMAIL_ADDR, RECV_ADDR, msg)
+		smtp.sendmail(EMAIL_ADDR, RECV_ADDR, MSG)
 
 
 # function executioner
-notify_user(EMAIL_ADDR, EMAIL_PASS, RECV_ADDR, SUBJECT, BODY)
+notify_user(EMAIL_ADDR, EMAIL_PASS, RECV_ADDR, EMAIL_SRVR, EMAIL_PORT, SUBJECT, BODY)
+print("message sent")
+
 
 
 
